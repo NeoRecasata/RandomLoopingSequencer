ad40415a754c8db9939875a0a850d4f315ae072a
Franco Neo Recasata <neorecasata@gmail.com>
2021-08-29 14:32:18 +0800

    Uploaded Latest Revision

--- Random_Looping_Sequencer.ino
+++ Random_Looping_Sequencer.ino
@@ -22,23 +22,24 @@
 
 Adafruit_MCP4725 dac;
 
-#define potPin A0
-#define pot2Pin A1
+#define potPin A0 //CV probability
+#define pot2Pin A1 //trigger probability
+#define pot3Pin A2 //step length
 #define clockPin 3
 #define pulsePin 4
 #define cvErasePin 5
 #define pulseSequenceErasePin 6
 #define trigLed 7
 
-
 int sequence[16]; //step array
 int pulseSequence[16]; //array of triggers/pulses
 int count = 0; //number of steps
 int randNum = 0;
+int randNum2 = 0;
 int steps = 16;
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200); //UNCOMMENT FOR DEBUGGING
   pinMode(pulsePin, OUTPUT);
   pinMode(trigLed, OUTPUT);
 
@@ -51,17 +52,15 @@ void setup() {
 }
 
 void loop() {
-
-
   do {
-    //generate a 12bit random number
-    randNum = random(0, 4096);
+
+    stepsLength(); //determine steps length
 
     if (digitalRead(clockPin) == LOW) {
 
       //check if cv erase switch is high
       cvErase();
-      
+
       //check if pulse erase switch is high
       pulseErase();
 
@@ -69,17 +68,17 @@ void loop() {
       changeStepCv();
 
       //Output cv
-      Serial.print(count);
-      Serial.print(" ");
+      //Serial.print(count); //UNCOMMENT FOR DEBUGGING
+      //Serial.print(" "); //UNCOMMENT FOR DEBUGGING
       dac.setVoltage(sequence[count], false);
-      Serial.println(sequence[count]);
+      //Serial.println(sequence[count]); //UNCOMMENT FOR DEBUGGING
 
       //change voltage of step depending on the pulse pot
       changeStepPulse();
 
       //Output pulse
       if (pulseSequence[count] == 1) {
-        Serial.println("pulse");
+        //Serial.println("pulse"); //UNCOMMENT FOR DEBUGGING
         digitalWrite(trigLed, HIGH);
         digitalWrite(pulsePin, HIGH);
         delay(12);
@@ -114,11 +113,13 @@ static void cvErase() {
 static void pulseErase() {
   //erase or set to 0 the current step if the erase pin is high
   if (digitalRead(pulseSequenceErasePin) == HIGH) {
-    sequence[count] = 0;
+    pulseSequence[count] = 0;
   }
 }
 
 static void changeStepCv() {
+  //generate a 12bit random number
+  randNum = random(0, 4096);
   //randomly change values in the array depending on the potentiometer
   if (getRandomBool(analogRead(potPin))) {
     sequence[count] = randNum;
@@ -126,9 +127,46 @@ static void changeStepCv() {
 }
 
 static void changeStepPulse() {
+  //generate random number from 0 to 1
+  randNum2 = random(0, 2);
   //randomly generate triggers based on the trigger potentiometer
   if (getRandomBool(analogRead(pot2Pin))) {
-    Serial.println(pulseSequence[count]);
-    pulseSequence[count] = 1;
+    //Serial.println(pulseSequence[count]); //UNCOMMENT FOR DEBUGGING
+    //Serial.println("pulse"); //UNCOMMENT FOR DEBUGGING
+    pulseSequence[count] = randNum2;
+  }
+}
+
+static void stepsLength() {
+  /* Predefined steps
+      2, 3, 4, 5, 6, 8, 12, 16
+  */
+
+  int potValue = analogRead(pot3Pin);
+
+  //Dividing the potentiometer to 8 different step lengths
+  if ((0 <= potValue) && (potValue <= 127)) {
+    steps = 2;
+  }
+  else if ((128 <= potValue) && (potValue <= 255)) {
+    steps = 3;
+  }
+  else if ((256 <= potValue) && (potValue <= 383)) {
+    steps = 4;
+  }
+  else if ((384 <= potValue) && (potValue <= 511)) {
+    steps = 5;
+  }
+  else if ((512 <= potValue) && (potValue <= 639)) {
+    steps = 6;
+  }
+  else if ((640 <= potValue) && (potValue <= 767)) {
+    steps = 8;
+  }
+  else if ((768 <= potValue) && (potValue <= 895)) {
+    steps = 12;
+  }
+  else if ((896 <= potValue) && (potValue <= 1023)) {
+    steps = 16;
   }
 }
